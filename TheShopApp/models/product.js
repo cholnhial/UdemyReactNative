701e57479c47dd7d545bd581f0d84a39f5123a73
Chol Nhial <chol.nhial.chol@gmail.com>
2022-01-31 10:59:10 +1000

    finish 168

--- TheShopApp/models/product.js
+++ TheShopApp/models/product.js
@@ -1,11 +1,13 @@
 class Product {
-    constructor(id, ownerId, title, imageUrl, description, price) {
+     constructor(id, ownerId, title, imageUrl, description, price) {
         this.id = id;
         this.ownerId = ownerId;
         this.imageUrl = imageUrl;
-        this.tittle = title;
+        this.title = title;
         this.description = description;
         this.price = price;
 
     }
 }
+
+export default Product;
--- TheShopApp/screens/shop/ProductsOverviewScreen.js
+++ TheShopApp/screens/shop/ProductsOverviewScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, Text } from 'react-native';
+import { useSelector } from "react-redux";
 
 const ProductsOverviewScreen = props => {
-    return
+    const products = useSelector(state => state.products.availableProducts);
+
+    return <FlatList data={products} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
 };
 
+ProductsOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products'
+}
 
 export default ProductsOverviewScreen;
