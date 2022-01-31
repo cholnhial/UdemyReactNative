8357c09954a5e455f2d280366c131e513fd6f6a4
Chol Nhial <chol.nhial.chol@gmail.com>
2022-01-31 10:36:23 +1000

    finish 166

---
+++ TheShopApp/models/product.js
@@ -0,0 +1,11 @@
+class Product {
+    constructor(id, ownerId, title, imageUrl, description, price) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.imageUrl = imageUrl;
+        this.tittle = title;
+        this.description = description;
+        this.price = price;
+
+    }
+}
---
+++ TheShopApp/screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+    return
+};
+
+
+export default ProductsOverviewScreen;
