1627e71efb05c41a959ffe2f148670c0cb5985ea
Chol Nhial <chol.nhial.chol@gmail.com>
2022-02-02 11:48:14 +1000

    finish 179

--- TheShopApp/screens/shop/OrdersScreen.js
+++ TheShopApp/screens/shop/OrdersScreen.js
@@ -3,11 +3,13 @@ import {FlatList, Platform, Text} from 'react-native';
 import { useSelector } from "react-redux";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = props => {
  const orders = useSelector(state => state.orders.orders);
 
- return <FlatList data={orders} renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>} />
+ return <FlatList data={orders} renderItem={itemData => <OrderItem amount={itemData.item.totalAmount}
+                                                                   date={itemData.item.readableDate} />} />
 };
 
 OrdersScreen.navigationOptions = navData => {
